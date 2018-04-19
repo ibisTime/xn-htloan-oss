d744ac6428bb6d5a6f7760b8f4d881f828c3581d
wulq <wlqsltz@163.com>
2018-04-19 14:40:41 +0800

    update

--- src/index.js
+++ src/index.js
@@ -15,7 +15,6 @@ const store = createStore(reducers, compose(
   applyMiddleware(thunk),
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 ));
-
 const Login = asyncComponent(() => import('container/login/login'));
 const Dashboard = asyncComponent(() => import('component/dashboard/dashboard'));
 
