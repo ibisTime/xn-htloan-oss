3e44ec1844f043768bfed9eb08eb054b9a45ab99
hss1994 <1070798770@qq.com>
2018-05-31 22:54:54 +0800

    平台银行调用修改

--- src/container/basedata/goodsloan/goodsloan-addedit.js
+++ src/container/basedata/goodsloan/goodsloan-addedit.js
@@ -14,6 +14,7 @@ import {
     DetailWrapper,
     beforeDetail
 } from 'common/js/build-detail';
+
 // import { COMPANY_CODE } from 'common/js/config';
 
 @DetailWrapper(
@@ -32,47 +33,48 @@ class goodsloanAddedit extends React.Component {
         this.code = getQueryString('code', this.props.location.search);
         this.view = !!getQueryString('v', this.props.location.search);
     }
+
     render() {
-      const fields = [{
-        title: '公证费',
-        field: 'authFee',
-        amount: true,
-        required: true
-      }, {
-        title: '服务费',
-        field: 'fee',
-        amount: true,
-        required: true
-      }, {
-        title: 'GPS费用',
-        field: 'gpsFee',
-        amount: true,
-        required: true
-      }, {
-        title: '贷款银行',
-        field: 'loanBank',
-        type: 'select',
-        listCode: 802116,
-        keyName: 'id',
-        valueName: 'bankName',
-        required: true
-      }, {
-        title: '月供利率',
-        field: 'monthRate',
-        required: true
-      }, {
-        title: '名称',
-        field: 'name',
-        required: true
-      }];
-      return this.props.buildDetail({
-        fields,
-        code: this.code,
-        view: this.view,
-        addCode: '632170',
-        editCode: '632172',
-        detailCode: '632176'
-      });
+        const fields = [{
+            title: '名称',
+            field: 'name',
+            required: true
+        }, {
+            title: '公证费',
+            field: 'authFee',
+            amount: true,
+            required: true
+        }, {
+            title: '服务费',
+            field: 'fee',
+            amount: true,
+            required: true
+        }, {
+            title: 'GPS费用',
+            field: 'gpsFee',
+            amount: true,
+            required: true
+        }, {
+            title: '贷款银行',
+            field: 'loanBank',
+            type: 'select',
+            listCode: 632037,
+            keyName: 'code',
+            valueName: '{{bankName.DATA}}{{subbranch.DATA}}',
+            required: true
+        }, {
+            title: '月供利率',
+            field: 'monthRate',
+            required: true
+        }];
+        return this.props.buildDetail({
+            fields,
+            code: this.code,
+            view: this.view,
+            addCode: '632170',
+            editCode: '632172',
+            detailCode: '632176'
+        });
     }
 }
 
--- src/container/loan/credit/credit.js
+++ src/container/loan/credit/credit.js
@@ -61,8 +61,8 @@ class Credit extends React.Component {
             field: 'loanBankCode',
             type: 'select',
             listCode: 632037,
-            keyName: 'bankCode',
-            valueName: 'bankName'
+            keyName: 'code',
+            valueName: '{{bankName.DATA}}{{subbranch.DATA}}'
         }, {
             title: '贷款金额',
             field: 'loanAmount',
