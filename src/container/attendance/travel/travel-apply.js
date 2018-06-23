22a0371348d97a45c1515cd658b770528fc71bad
U55W8NHWI71RNAP\Administrator <544741767@qq.com>
2018-06-23 16:39:05 +0800

    update

--- src/container/attendance/travel/travel-apply.js
+++ src/container/attendance/travel/travel-apply.js
@@ -18,11 +18,107 @@ import fetch from 'common/js/fetch';
 class TravelApply extends React.Component {
     render() {
         const fields = [{
-            title: '类型',
-            field: 'type',
-            value: '1',
-            hidden: true,
-            required: true
+            title: '出差申请信息',
+            items: [
+                [{
+                    title: '出差人',
+                    field: ''
+                }, {
+                    title: '出差时间',
+                    field: '',
+                    type: 'date'
+                }],
+                [{
+                    title: '飞机票费用标准',
+                    field: '',
+                    amount: true
+                }, {
+                    title: '飞机票天数',
+                    field: ''
+                }, {
+                    title: '飞机票预算金额',
+                    field: '',
+                    amount: true
+                }],
+                [{
+                    title: '火车票费用标准',
+                    field: '',
+                    amount: true
+                }, {
+                    title: '火车票天数',
+                    field: ''
+                }, {
+                    title: '火车票预算金额',
+                    field: '',
+                    amount: true
+                }],
+                [{
+                    title: '市内交通费用标准',
+                    field: '',
+                    amount: true
+                }, {
+                    title: '市内交通天数',
+                    field: ''
+                }, {
+                    title: '市内交通预算金额',
+                    field: '',
+                    amount: true
+                }],
+                [{
+                    title: '住宿费',
+                    field: '',
+                    amount: true
+                }, {
+                    title: '伙食补助',
+                    field: '',
+                    amount: true
+                }, {
+                    title: '招待费',
+                    field: '',
+                    amount: true
+                }],
+                [{
+                    title: '其他（详细明细）',
+                    field: 'other',
+                    type: 'textarea',
+                    normalArea: true,
+                }],
+                [{
+                    title: '小计',
+                    field: '',
+                    amount: true
+                }, {
+                    title: '备用金',
+                    field: '',
+                    amount: true
+                }, {
+                    title: '费用合计',
+                    field: '',
+                    amount: true
+                }],
+                [{
+                    title: '金额大写',
+                    field: 'other'
+                }],
+                [{
+                    title: '出差事由',
+                    field: 'other',
+                    type: 'textarea',
+                    normalArea: true,
+                }],
+                [{
+                    title: '出差路线',
+                    field: 'other',
+                    type: 'textarea',
+                    normalArea: true,
+                }],
+                [{
+                    title: '备注',
+                    field: 'other',
+                    type: 'textarea',
+                    normalArea: true,
+                }]
+            ]
         }];
         return this.props.buildDetail({
             fields,
