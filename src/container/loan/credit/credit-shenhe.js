9eff83e386f513410d2daf57481be15dc7a040c4
XLF <1574425015@qq.com>
2019-04-16 15:54:01 +0800

    准入审查 风控一、二审核

--- src/container/loan/credit/credit-shenhe.js
+++ src/container/loan/credit/credit-shenhe.js
@@ -5,10 +5,16 @@ import { getQueryString, showWarnMsg, showSucMsg, getUserId, moneyFormat } from 
 import { DetailWrapper } from 'common/js/build-detail';
 import fetch from 'common/js/fetch';
 
+// 是否通过
+// const bankCreditResultPdf = [
+//     {k: '0', v: '否'},
+//     {k: '1', v: '是'}
+// ];
 @DetailWrapper(
     state => state.loanCreditAddedit,
     {initStates, doFetching, cancelFetching, setSelectData, setPageData, restore}
 )
+
 class CreditAddedit extends React.Component {
     constructor(props) {
         super(props);
@@ -30,94 +36,20 @@ class CreditAddedit extends React.Component {
         this.creditUserListIndex = 6;
         this.buttons = [];
         this.concatFalg = false;
+        this.creditUserList = [];
     }
     render() {
         // 征信列表字段
-        let o2mFields = [{
-            title: '姓名',
-            field: 'userName',
-            nowrap: true,
-            required: true,
-            width: 80
-        }, {
-            title: '与借款人关系',
-            field: 'relation',
-            type: 'select',
-            key: 'credit_user_relation',
-            required: true
-        }, {
-            title: '贷款角色',
-            field: 'loanRole',
-            type: 'select',
-            key: 'credit_user_loan_role',
-            required: true,
-            onChange: (v, data, props) => {
-                let creditUserList = this.props.pageData.creditUserList;
-                let loanRoleList = {};
-                props.selectData.loanRole.map(l => {
-                    loanRoleList[l.dkey] = l.dvalue;
-                });
-                creditUserList && creditUserList.forEach(d => {
-                    if (d.loanRole === v) {
-                        setTimeout(() => {
-                            props.form.setFieldsValue({loanRole: ''});
-                        }, 100);
-                        showWarnMsg(loanRoleList[v] + '记录已添加');
-                        return false;
-                    }
-                });
-            }
-        }, {
-            title: '手机号',
-            field: 'mobile',
-            mobile: true,
-            required: true,
-            render: (v) => {
-                let val = (v && v.replace(/^(\d{3})\d{4}(\d{4})$/, '$1****$2')) || '';
-                return <span style={{whiteSpace: 'nowrap'}}>{val}</span>;
-            }
-        }, {
-            title: '身份证号',
-            field: 'idNo',
-            idCard: true,
-            required: true,
-            render: (v) => {
-                let val = (v && v.replace(/^(\d{6}).+(\d{4})$/, '$1****$2')) || '';
-                return <span style={{whiteSpace: 'nowrap'}}>{val}</span>;
-            }
-        }, {
-            title: '身份证正面',
-            field: 'idNoFront',
-            type: 'img',
-            single: true,
-            required: true,
-            hidden: this.isEntry
-        }, {
-            title: '身份证反面',
-            field: 'idNoReverse',
-            type: 'img',
-            single: true,
-            required: true,
-            hidden: this.isEntry
-        }, {
-            title: '征信查询授权书',
-            field: 'authPdf',
-            type: 'img',
-            required: true,
-            hidden: this.isEntry
-        }, {
-            title: '面签照片',
-            field: 'interviewPic',
-            type: 'img',
-            required: true,
-            hidden: this.isEntry
-        }];
+        // let o2mFields = [];
 
         // 详情回显列表字段
         let fields = [{
             title: '业务编号',
             field: 'code',
             formatter: (v, d) => {
+                if(d.creditUserList) {
+                    this.creditUserList = d.creditUserList; // 存储creditUserList
+                }
                 return d ? d.cdbiz.code : '';
             }
         }, {
@@ -168,54 +100,153 @@ class CreditAddedit extends React.Component {
                 return d ? d.cdbiz.status : '';
             }
         }, {
-            title: '审核说明',
-            field: 'approveNote',
-            readonly: !this.isCheck,
-            hidden: !this.isCheck
-        }, {
-            title: '主贷人征信',
-            field: 'creditUserList',
-            readonly: false,
-            hidden: this.isCheck,
-            options: {
-                detail: !(this.isEntry || !this.view),
-                check: this.isEntry,
-                normalBtn: this.isCheck,
-                normalBtnName: '角色互换',
-                normalHandler: (keys) => {
-                    let list = this.props.pageData.creditUserList.slice();
-                    if (!keys.length) {
-                        showWarnMsg('请选择记录');
-                    } else if (keys.length !== 2) {
-                        showWarnMsg('请选择两条记录');
-                    } else {
-                        let idx0 = list.findIndex(l => l.code === keys[0]);
-                        let item0 = list[idx0];
-                        let idx1 = list.findIndex(l => l.code === keys[1]);
-                        let item1 = list[idx1];
-                        if (item0.loanRole !== '1' && item1.loanRole !== '1') {
-                            showWarnMsg('其中一条必须选择申请人');
-                            return;
-                        }
-                        list.splice(idx0, 1, {
-                            ...item0,
-                            loanRole: item1.loanRole,
-                            relation: item1.relation
-                        });
-                        list.splice(idx1, 1, {
-                            ...item1,
-                            loanRole: item0.loanRole,
-                            relation: item0.relation
-                        });
-                        this.props.setPageData({
-                            ...this.props.pageData,
-                            creditUserList: list
-                        });
-                    }
-                },
-                scroll: {x: 1300},
-                fields: o2mFields
+            title: '姓名',
+            field: ' ',
+            nowrap: true,
+            required: true,
+            formatter: (v, d) => {
+                return d ? d.creditUserList[0].userName : '';
             }
+        }, {
+            title: '与借款人关系',
+            field: 'relation',
+            type: 'select',
+            key: 'credit_user_relation',
+            required: true,
+            formatter: (v, d) => {
+                return d ? d.creditUserList[0].relation : '';
+            }
+        }, {
+            title: '贷款角色',
+            field: 'loanRole',
+            type: 'select',
+            key: 'credit_user_loan_role',
+            required: true,
+            formatter: (v, d) => {
+                return d ? d.creditUserList[0].loanRole : '';
+            },
+            onChange: (v, data, props) => {
+                let creditUserList = this.props.pageData.creditUserList;
+                let loanRoleList = {};
+                props.selectData.loanRole.map(l => {
+                    loanRoleList[l.dkey] = l.dvalue;
+                });
+                creditUserList && creditUserList.forEach(d => {
+                    if (d.loanRole === v) {
+                        setTimeout(() => {
+                            props.form.setFieldsValue({loanRole: ''});
+                        }, 100);
+                        showWarnMsg(loanRoleList[v] + '记录已添加');
+                        return false;
+                    }
+                });
+            }
+        }, {
+            title: '手机号',
+            field: 'mobile',
+            mobile: true,
+            required: true,
+            formatter: (v, d) => {
+                return d ? d.creditUserList[0].mobile : '';
+            },
+            render: (v) => {
+                let val = (v && v.replace(/^(\d{3})\d{4}(\d{4})$/, '$1****$2')) || '';
+                return <span style={{whiteSpace: 'nowrap'}}>{val}</span>;
+            }
+        }, {
+            title: '身份证号',
+            field: 'idNo',
+            idCard: true,
+            required: true,
+            formatter: (v, d) => {
+                return d ? d.creditUserList[0].idNo : '';
+            },
+            render: (v) => {
+                let val = (v && v.replace(/^(\d{6}).+(\d{4})$/, '$1****$2')) || '';
+                return <span style={{whiteSpace: 'nowrap'}}>{val}</span>;
+            }
+        }, {
+            title: '身份证正反面',
+            field: 'idNo1',
+            type: 'img',
+            // required: true,
+            readonly: true,
+            key: 'attachment_name',
+            formatter: (v, d) => {
+                return d ? d.attachments.url : '';
+            }
+        }, {
+            title: '征信查询授权书',
+            field: 'name',
+            type: 'select',
+            // required: true,
+            readonly: true,
+            key: 'attachment_name'
+        }, {
+            title: '面签照片',
+            field: 'interviewPic',
+            type: 'img',
+            single: true,
+            // required: true,
+            readonly: true,
+            key: 'attachment_name',
+            formatter: (v, d) => {
+                return d ? d.attachments.url : '';
+            }
+        }, {
+            title: '银行征信报告',
+            field: 'bankCreditReport',
+            type: 'img',
+            single: true, // 单张
+            // required: true,
+            readonly: true,
+            key: 'attachment_name',
+            formatter: (v, d) => {
+                return d ? d.attachments.url : '';
+            }
+        }, {
+            title: '银行征信结果是否通过',
+            field: 'bankCreditResultPdf',
+            type: 'select',
+            readonly: true,
+            data: [{
+                dkey: '0',
+                dvalue: '否'
+            }, {
+                dkey: '1',
+                dvalue: '是'
+            }],
+            keyName: 'dkey',
+            valueName: 'dvalue',
+            formatter: (v, d) => {
+                return d ? d.creditUserList[0].bankCreditResultPdf : '';
+            }
+        }, {
+            title: '大数据征信报告(多张)',
+            field: 'dataCreditReport',
+            type: 'img',
+            // required: true,
+            readonly: true,
+            key: 'attachment_name',
+            formatter: (v, d) => {
+                return d ? d.attachments.url : '';
+            }
+        }, {
+            title: '征信报告说明',
+            field: 'bankCreditResultRemark',
+            required: true,
+            readonly: true,
+            type: 'textarea',
+            normalArea: true,
+            formatter: (v, d) => {
+                return d ? d.creditUserList[0].bankCreditResultRemark : '';
+            }
+        }, {
+            title: '审核意见',
+            field: 'approveNote',
+            type: 'textarea',
+            normalArea: true,
+            readonly: false
         }];
         // 风控专员审核
         if (this.isCheck) {
@@ -223,16 +254,26 @@ class CreditAddedit extends React.Component {
                 title: '通过',
                 check: true,
                 handler: (params) => {
+                    console.log(params, this.creditUserList);
                     let data = {};
-                    data.code = this.code;
-                    data.approveNote = params.approveNote;
-                    data.approveResult = '1';
+                    let item = [];
+                    data.code = this.code; // 征信单编号
+                    data.approveNote = params.approveNote; // 审核说明
+                    data.approveResult = '1';// 审核结果
                     data.operator = getUserId();
-                    if (!params.creditUserList || params.creditUserList.length < 1) {
-                        showWarnMsg('至少录入一条征信人员信息！');
-                        return;
+                    // if (!params.creditUserList || params.creditUserList.length < 1) {
+                    //     showWarnMsg('至少录入一条征信人员信息！');
+                    //     return;
+                    // }
+                    for (let i = 0; i < this.creditUserList.length; i++) {
+                        this.creditUserList[i].creditUserCode = this.creditUserList[i].code;
+                        item.push({
+                            code: this.creditUserList[i].creditUserCode,
+                            loanRole: this.creditUserList[i].loanRole,
+                            relation: this.creditUserList[i].relation
+                        });
                     }
-                    data.creditUserList = this.props.pageData.creditUserList;
+                    data.creditUserList = item;
                     this.props.doFetching();
                     fetch(632113, data).then(() => {
                         showSucMsg('操作成功');
@@ -246,6 +287,7 @@ class CreditAddedit extends React.Component {
                 title: '不通过',
                 check: true,
                 handler: (params) => {
+                    console.log(params);
                     let data = {};
                     data.code = this.code;
                     data.approveNote = params.approveNote;
