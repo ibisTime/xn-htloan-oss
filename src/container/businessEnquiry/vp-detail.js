133dcaf7e5f632f4284e275a6f427c64cd505a32
John Doe <johndoe@example.com>
2019-12-06 13:42:59 +0800

    温州，合同版本oss上传

--- src/container/businessEnquiry/vp-detail.js
+++ src/container/businessEnquiry/vp-detail.js
@@ -14,6 +14,7 @@ import {Row, Col, Layout} from 'antd';
 import { Switch, Route, Link } from 'react-router-dom';
 import asyncComponent from '../../component/async-component/async-component';
 import './detail.css';
+import fetch from 'common/js/fetch';
 const { Content } = Layout;
 
 // 准入详情
@@ -34,11 +35,20 @@ class vpDetail extends React.Component {
             item2: false,
             item3: false,
             item4: false,
-            item5: false
+            item5: false,
+            isShowNot: {}
         };
         this.code = getQueryString('code', this.props.location.search);
         this.cpt = admittance;
     }
+    componentDidMount(): void {
+        fetch(632529, {code: this.code}).then(data => {
+            this.setState({
+                isShowNot: data
+            });
+        });
+    }
+
     setInOrOut = (value) => {
         switch (value) {
             case 'item1':
@@ -94,15 +104,27 @@ class vpDetail extends React.Component {
         }
     }
     render() {
-        const {item1, item2, item3, item4, item5} = this.state;
+        const {item1, item2, item3, item4, item5, isShowNot} = this.state;
         return (
             <div style={{background: '#fff'}}>
                 <div className="contain-header-nav">
                     <span className={item1 ? 'contain-header-nav-item-in' : 'contain-header-nav-item-out'} onClick={(value) => this.setInOrOut('item1')}>准入详情</span>
                     <span className={item2 ? 'contain-header-nav-item-in' : 'contain-header-nav-item-out'} onClick={(value) => this.setInOrOut('item2')}>垫资详情</span>
-                    <span className={item3 ? 'contain-header-nav-item-in' : 'contain-header-nav-item-out'} onClick={(value) => this.setInOrOut('item3')}>理件详情</span>
-                    <span className={item4 ? 'contain-header-nav-item-in' : 'contain-header-nav-item-out'} onClick={(value) => this.setInOrOut('item4')}>放款详情</span>
-                    <span className={item5 ? 'contain-header-nav-item-in' : 'contain-header-nav-item-out'} onClick={(value) => this.setInOrOut('item5')}>入档详情</span></div>
+                    {
+                        isShowNot.mindWare === '1' ? (
+                            <span className={item3 ? 'contain-header-nav-item-in' : 'contain-header-nav-item-out'} onClick={(value) => this.setInOrOut('item3')}>理件详情</span>
+                        ) : null
+                    }
+                    {
+                        isShowNot.bankFk === '1' ? (
+                            <span className={item4 ? 'contain-header-nav-item-in' : 'contain-header-nav-item-out'} onClick={(value) => this.setInOrOut('item4')}>放款详情</span>
+                        ) : null
+                    }
+                    {
+                        isShowNot.enter === '1' ? (
+                            <span className={item5 ? 'contain-header-nav-item-in' : 'contain-header-nav-item-out'} onClick={(value) => this.setInOrOut('item5')}>入档详情</span>                        ) : null
+                    }
+                </div>
                 <Layout>
                     <Content>
                         <Switch>
