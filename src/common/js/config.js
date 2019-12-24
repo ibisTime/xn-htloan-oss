2e4026ccdcaec2e1c496dc29c01a825007a170e4
John Doe <johndoe@example.com>
2019-12-24 14:58:34 +0800

    温州，310

--- src/common/js/config.js
+++ src/common/js/config.js
@@ -1,9 +1,9 @@
 export const SYSTEM_CODE = 'CD-HTWT000020';
 // export const COMPANY_CODE = 'CD-COIN000017';
 // 七牛上传地址
-export const UPLOAD_URL = 'http://img.wzhaoyuan.com';
+export const UPLOAD_URL = 'http://up-z0.qiniup.com';
 // 七牛图片前缀
-export const PIC_PREFIX = 'http://img.fhcdzx.com/';
+export const PIC_PREFIX = 'http://img.wzhaoyuan.com/';
 export const PIC_BASEURL_M = '?imageMogr2/auto-orient/thumbnail/!200x200';
 export const PIC_BASEURL_L = '?imageMogr2/auto-orient/thumbnail/!1000x1000r';
 
